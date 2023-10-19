4ace85d3f3d6d5849ea3bcc5ccd3dcc71e8d8349
venelinkolev <venelinkolev@gmail.com>
2023-10-19 17:42:48 +0300

    Home Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,33 +4,41 @@ import { DocumentComponent } from './feature/document/document.component';
 import { InvoiceDocumentsComponent } from './feature/invoice-documents/invoice-documents.component';
 import { CustomerOrdersComponent } from './feature/customer-orders/customer-orders.component';
 import { ProformaInvoiceComponent } from './feature/proforma-invoice/proforma-invoice.component';
+import { HomeComponent } from './home-page/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: '/documents',
+    // redirectTo: '/documents',
+    redirectTo: '/home',
   },
   {
-    path: 'documents',
-    title: 'Documents',
-    component: DocumentComponent,
+    path: 'home',
+    title: 'Home',
+    component: HomeComponent,
   },
-  {
-    path: 'invoice',
-    title: 'Invoice',
-    component: InvoiceDocumentsComponent,
-  },
-  {
-    path: 'customer-orders',
-    title: 'Customer Orders',
-    component: CustomerOrdersComponent,
-  },
-  {
-    path: 'proforma-invoice',
-    title: 'Proforma Invoice',
-    component: ProformaInvoiceComponent,
-  },
+
+  // {
+  //   path: 'documents',
+  //   title: 'Documents',
+  //   component: DocumentComponent,
+  // },
+  // {
+  //   path: 'invoice',
+  //   title: 'Invoice',
+  //   component: InvoiceDocumentsComponent,
+  // },
+  // {
+  //   path: 'customer-orders',
+  //   title: 'Customer Orders',
+  //   component: CustomerOrdersComponent,
+  // },
+  // {
+  //   path: 'proforma-invoice',
+  //   title: 'Proforma Invoice',
+  //   component: ProformaInvoiceComponent,
+  // },
 ];
 
 @NgModule({
